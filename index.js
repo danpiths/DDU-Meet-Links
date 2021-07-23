a22442320139b51dfd595882085352886d30f49c
Dan Piths <danpiths@gmail.com>
2021-07-23 18:23:59 +0530

    Created Javascript functionality

---
+++ index.js
@@ -0,0 +1,75 @@
+// VARIABLE DECLARATION
+const siteTitle = document.querySelector(".site-title");
+const linkCollectorForm = document.getElementById("link-collector");
+const formSubmitted = JSON.parse(localStorage.getItem("formSubmitted"));
+const links = JSON.parse(localStorage.getItem("links"));
+const currentDate = new Date();
+const currentDayNum = currentDate.getDay();
+const numToDay = {
+  0: null,
+  1: ["Monday", links.mondayLink],
+  2: ["Tuesday", links.tuesdayLink],
+  3: ["Wednesday", links.wednesdayLink],
+  4: ["Thursday", links.thursdayLink],
+  5: ["Friday", links.fridayLink],
+  6: null,
+};
+
+// FUNCTIONS
+const collectDataFromLinkCollector = (event) => {
+  event.preventDefault();
+  const linkFormData = new FormData(event.target);
+  const name = linkFormData.get("name");
+  const mondayLink = linkFormData.get("mondayLink");
+  const tuesdayLink = linkFormData.get("tuesdayLink");
+  const wednesdayLink = linkFormData.get("wednesdayLink");
+  const thursdayLink = linkFormData.get("thursdayLink");
+  const fridayLink = linkFormData.get("fridayLink");
+  const mainLink = linkFormData.get("mainLink");
+  localStorage.setItem("formSubmitted", JSON.stringify(true));
+  const links = {
+    name: name,
+    mondayLink: mondayLink,
+    tuesdayLink: tuesdayLink,
+    wednesdayLink: wednesdayLink,
+    thursdayLink: thursdayLink,
+    fridayLink: fridayLink,
+    mainLink: mainLink,
+  };
+  localStorage.setItem("links", JSON.stringify(links));
+};
+
+const generateMeetButtons = (htmlDiv) => {
+  const mainLink = links.mainLink;
+  if (0 < currentDayNum < 6) {
+    const dayName = numToDay[currentDayNum][0];
+    const dayLink = numToDay[currentDayNum][1];
+    htmlDiv.innerHTML = `
+      <p class="display-text">Here are your links for ${dayName}</p>
+      <a class="link lab-link" href="${dayLink}">${dayName} Lab</a>
+      <a class="link main-link" href="${mainLink}">Main Lecture</a>
+    `;
+  } else if (currentDayNum == 6) {
+    htmlDiv.innerHTML = `
+      <p class="display-text">You have NO Lab today</p>
+      <a class="link main-link" href="${mainLink}">Main Lecture</a>
+    `;
+  } else if (currentDayNum == 0) {
+    siteTitle.textContent = "It's a holiday today, enjoy bitch!";
+  }
+};
+
+// PROGRAMS STARTS
+if (formSubmitted) {
+  linkCollectorForm.style.display = "none";
+  siteTitle.classList.add("submitted-title");
+  siteTitle.textContent = `Hello ${links.name}`;
+  const htmlDiv = document.createElement("div");
+  document.body.appendChild(htmlDiv);
+  htmlDiv.classList.add("link-container");
+  generateMeetButtons(htmlDiv);
+} else {
+  linkCollectorForm.addEventListener("submit", (event) => {
+    collectDataFromLinkCollector(event);
+  });
+}
